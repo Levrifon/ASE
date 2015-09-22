196ea9a8ef285cacd851b3d38f37622b36c9131e
Levrifon <rems-msn@hotmail.fr>
2015-09-22 12:00:48 +0200

    gitignore and try_mul.c

---
+++ TP1/try_mul.c
@@ -0,0 +1,29 @@
+#include <setjmp.h>
+#include <stdio.h>
+static int mul(int depth) {
+	int i;
+	switch(scanf("%d", &i)) {
+		case EOF :
+			return 1; /*neutral element */
+		case 0 :
+			return mul(depth+1);
+		case 1 :
+			if(i)
+				return i * mul(depth+1);
+			else
+				return 0;
+	}
+
+}
+
+int main() {
+	int product;
+	static jump_buf buf;
+	printf("A list of int, please ! \n");
+	if(setjmp(buf)) {
+		product = 0;
+	} else {
+		product = mul(0);
+	}
+
+}
