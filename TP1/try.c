248d9f0f6ca36a0ed85e82ab77941a2cec999c96
Théo Plockyn <theo.plockyn@gmail.com>
2015-09-26 14:00:01 +0200

    Tried something, didn't worked

--- TP1/try.c
+++ TP1/try.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "try.h"
 
+static int res;
+
 int try(struct ctx_s *pctx, func_t *f, int arg) {
     
     asm("movl %%esp, %0" "\n\t" "movl %%ebp, %1"
@@ -8,16 +10,15 @@ int try(struct ctx_s *pctx, func_t *f, int arg) {
             :
             :);
 
-
     return f(arg);
 }
 
 int throw(struct ctx_s *pctx, int r) {
-       
+    res = r;
     asm("movl %0, %%esp" "\n\t" "movl %1, %%ebp"
             :
             :"r"(pctx->ctx_esp), "r"(pctx->ctx_ebp)
             :);
- 
-    return r;
+
+    return res;
 }
