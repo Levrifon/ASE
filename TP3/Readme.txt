96f8de5b1d2e220ac6e0f90f97c962b46b6aea18
Théo Plockyn <theo.plockyn@gmail.com>
2015-10-06 11:57:38 +0200

    TP3 fini

---
+++ TP3/Readme.txt
@@ -0,0 +1,17 @@
+Rendu TP3 :
+
+DEBUE Remy et PLOCKYN Theo
+
+Les fichiers utilisés sont :
+______________________________
+
+entete.h (déclaration des fonctions)
+lib.c    (notre librairie de fonctions)
+main.c   (déroule le programme avec les fonctions ping pong pang)
+
+Difficultés rencontrées :
+______________________________
+
+- Gestion de l'arrêt des contextes qui a nécessité un peu de reflexion
+
+
---
+++ TP3/main.c
@@ -0,0 +1,49 @@
+#include "entete.h"
+
+void f_ping(void *arg);
+void f_pong(void *arg);
+void f_pang(void *arg);
+
+int main(int argc, char* argv[]) {
+	create_ctx(16384,f_ping,NULL);
+	create_ctx(16384,f_pong,NULL);
+	create_ctx(16384,f_pang,NULL);
+	yield();
+	printf("Fini !\n");
+	exit(EXIT_SUCCESS);
+}
+
+void f_ping(void *args) {
+	int i;
+	for(i=0; i<100; i++){
+		printf("A");
+		yield();
+		printf("B");
+		yield();
+		printf("C");
+		yield();
+	}
+	printf("\nfini ping\n");
+}
+
+void f_pong(void *args) {
+	int i;
+	for(i=0; i<300; i++){
+		printf("1");
+		yield();
+		printf("2");
+		yield();
+	}
+	printf("\nfini pong\n");
+}
+
+void f_pang(void *args) {
+	int i;
+	for(i=0; i<200; i++){
+		printf("$");
+		yield();
+		printf("£");
+		yield();
+	}
+	printf("\nfini pang\n");
+}
