8688c4d730dc57f7a22a306ad46a4eddfa851263
Théo Plockyn <theo.plockyn@gmail.com>
2016-01-15 08:51:45 +0100

    ordonnanceur

---
+++ ordo/Readme.txt
@@ -0,0 +1,23 @@
+Rendu TP2 :
+
+DEBUE Remy et PLOCKYN Theo
+
+Les fichiers utilisés sont :
+______________________________
+
+entete.h (déclaration des fonctions)
+init_ctx.c (initialisation du contexte)
+switch_to.c (change l'état du contexte ou fait la transition entre deux contexte)
+main.c (déroule le programme avec les fonctions ping pong pang)
+
+Difficultés rencontrées :
+______________________________
+
+- Inversion d'instructions en assembleur, mauvais calcul de cas de départ (lorsuqu'on lance la fonction pour la première fois)
+- Mauvaise gestion de la troisième fonction (pang) mais rapidement corrigé
+
+Autres remarques :
+______________________________
+
+La bonne division des tâches entre les membres du binôme nous a permis d'avancer plutôt rapidement.
+
---
+++ ordo/entete.h
@@ -0,0 +1,35 @@
+#ifndef ENTETE
+#define ENTETE
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+enum ctx_state_e {CTX_INIT,CTX_EXE,CTX_END, CTX_BL_SEM};
+#define CTX_MAGIC 0xDEADC0DE
+typedef void (funct_t)(void*);
+
+struct ctx_s {
+	void* ctx_esp;
+	void* ctx_ebp;
+	int stack_size;
+	funct_t * ctx_f;
+	char* ctx_base;
+	void* ctx_args;
+	enum ctx_state_e ctx_state;
+	unsigned int ctx_magic; /* detrompeur */
+	struct ctx_s* next_ctx;
+	struct ctx_s * next_sem_bl;
+};
+
+static struct ctx_s * ctx_ring;
+static struct ctx_s * curr_ctx;
+static struct ctx_s * main_ctx;
+
+extern void init_ctx(struct ctx_s* ctx,int stack_size, funct_t * f, void* args);
+extern void yield();
+extern void create_ctx(int stacksize, funct_t * f, void * args);
+extern void switch_to(struct ctx_s * ctx);
+
+#endif // ENTETE
